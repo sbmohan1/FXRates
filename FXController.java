d802dba38c713e94efbdf75c11b0e6402c241b2d
Breadamonium <yp89@cornell.edu>
2017-08-03 16:12:30 -0400

    Fixed XML ordering problem
    Created LinkedProperties class extending Properties and linkedhashmap to preserve order of FXrates put in, so XML is ordered nicely.

--- FXController.java
+++ FXController.java
@@ -1,9 +1,10 @@
 package training.web.rest;
 
+import java.beans.XMLEncoder;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,17 +12,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import training.web.rest.LinkedProperties;
 
 @RestController
 @RequestMapping(value = "/FXRate")
 public class FXController {
-	
-	@RequestMapping(value = "rates", method = RequestMethod.GET, produces = {MediaType.TEXT_PLAIN_VALUE})
-	public ResponseEntity<String> getRates() throws IOException	{
-		
-    	Properties rates= new Properties();
-    	
-    	rates.put("usdUSD", "1.00000");
+
+	public static String maptoXML(Object hashMap) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		XMLEncoder xmlEncoder = new XMLEncoder(bos);
+		xmlEncoder.writeObject(hashMap);
+		xmlEncoder.close();
+		return bos.toString();
+	}
+
+	@RequestMapping(value = "rates", method = RequestMethod.GET, produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> getRates() throws IOException {
+
+		//LinkedHashMap<String, String> rates = new LinkedHashMap<String, String>();
+
+		FileOutputStream ratesFOS = new FileOutputStream("rates.xml");
+		FileInputStream ratesFIS = new FileInputStream("rates.xml");
+		LinkedProperties rates = new LinkedProperties();
+
+		rates.put("usdUSD", "1.00000");
 		rates.put("usdEUR", "0.85783");
 		rates.put("usdGBP", "0.76696");
 		rates.put("usdINR", "64.4390");
@@ -30,7 +44,7 @@ public class FXController {
 		rates.put("usdZAR", "13.0349");
 		rates.put("usdNZD", "1.34628");
 		rates.put("usdJPY", "111.644");
-		
+
 		rates.put("eurUSD", "1.16569");
 		rates.put("eurEUR", "1.00000");
 		rates.put("eurGBP", "0.89404");
@@ -40,7 +54,7 @@ public class FXController {
 		rates.put("eurZAR", "15.1964");
 		rates.put("eurNZD", "1.56946");
 		rates.put("eurJPY", "130.128");
-		
+
 		rates.put("gbpUSD", "1.30372");
 		rates.put("gbpEUR", "1.11849");
 		rates.put("gbpGBP", "1.00000");
@@ -51,18 +65,15 @@ public class FXController {
 		rates.put("gbpNZD", "1.75534");
 		rates.put("gbpJPY", "145.520");
 		
-		
-		FileOutputStream ratesFOS = new FileOutputStream("rates.xml");
-		FileInputStream ratesFIS = new FileInputStream("rates.xml");
 		rates.storeToXML(ratesFOS, "Global FXRates Test Data", "UTF-8");
 
 		StringBuilder ratesString = new StringBuilder();
-		
-		while (ratesFIS.available() > 0)	{
+
+		while (ratesFIS.available() > 0) {
 			ratesString.append("" + (char) ratesFIS.read());
 		}
 		ratesFIS.close();
-		
+
 		return new ResponseEntity<String>(ratesString.toString(), HttpStatus.OK);
 	}
 
---
+++ LinkedProperties.java
@@ -0,0 +1,54 @@
+package training.web.rest;
+
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+
+public class LinkedProperties extends Properties{
+
+
+    private static final long serialVersionUID = 1L;
+
+    private Map<Object, Object> linkMap = new LinkedHashMap<Object,Object>();
+
+    @Override
+    public synchronized Object put(Object key, Object value){
+        return linkMap.put(key, value);
+    }
+
+    @Override
+    public synchronized boolean contains(Object value){
+        return linkMap.containsValue(value);
+    }
+
+    @Override
+    public boolean containsValue(Object value){
+        return linkMap.containsValue(value);
+    }
+
+    @Override
+    public synchronized Enumeration<Object> elements(){
+        throw new UnsupportedOperationException(
+          "Enumerations are so old-school, don't use them, "
+        + "use keySet() or entrySet() instead");
+    }
+
+    @Override
+    public Set<Entry<Object, Object>> entrySet(){
+        return linkMap.entrySet();
+    }
+
+    @Override
+    public synchronized void clear(){
+        linkMap.clear();
+    }
+
+    @Override
+    public synchronized boolean containsKey(Object key){
+        return linkMap.containsKey(key);
+    }
+
+}
